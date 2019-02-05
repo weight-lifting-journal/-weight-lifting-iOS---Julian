e293e0e48e4de67a0e1114ff5979dd4bcf31f47f
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-02-05 00:07:48 -0500

    update layout

--- Weight Lift/Weight Lift/EntriesViewController.swift
+++ Weight Lift/Weight Lift/EntriesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EntriesViewController: UIViewController {
+class EntriesDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
